8bb7f44e180827534bc3d10a4d7c6cd1e4f0d162
jumaniyozov <jumaniyozovse@gmail.com>
2021-01-02 01:07:58 +0500

    pipes initial

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { Task, TaskStatus } from "./tasks.model";
 import { v4 as uuid } from "uuid";
 import { CreateTaskDto } from "./dto/create-task.dto";
@@ -12,13 +12,18 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find(task => task.id === id);
+    const found = this.tasks.find(task => task.id === id);
+
+    if(!found) {
+      throw new NotFoundException(`Task with ID ${id} not found!`);
+    }
+
+    return found;
   }
 
   deleteTaskById(id: string): void {
-    const startIndex = this.tasks.findIndex(task => task.id === id);
-    this.tasks.splice(startIndex, 1);
-    return;
+    const found = this.getTaskById(id);
+    this.tasks = this.tasks.filter(task => task.id !== id);
   }
 
   updateTaskById(id: string, data: object): Task {
@@ -29,7 +34,7 @@ export class TasksService {
 
   updateTaskStatus(id: string, status: TaskStatus): Task {
     const task =  this.getTaskById(id);
-    task.status = status
+    task.status = status;
     return task;
   }
 
