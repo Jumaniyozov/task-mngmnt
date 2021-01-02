1d4146ccf5953d6d32970543d88c59e17d96994e
jumaniyozov <jumaniyozovse@gmail.com>
2021-01-03 02:39:26 +0500

    deployment

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,3 @@
-import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -7,15 +6,17 @@ import { UserRepository } from "./user.repository";
 import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 import { JwtStrategy } from "./jwt.strategy";
+import * as config from 'config';
 
+const jwtConfig = config.get('jwt');
 
 @Module({
   imports: [
     PassportModule.register({defaultStrategy: 'jwt'}),
     JwtModule.register({
-      secret: process.env["JWT_SECRET"],
+      secret: process.env.JWT_SECRET || jwtConfig.secret,
       signOptions: {
-        expiresIn: 3600 * 6,
+        expiresIn: jwtConfig.expiresIn,
       }
     }),
     TypeOrmModule.forFeature([UserRepository])],
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
-import { NestFactory } from '@nestjs/core';
-import {Logger} from "@nestjs/common";
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { Logger } from "@nestjs/common";
+import { AppModule } from "./app.module";
+import * as config from "config";
 
 async function bootstrap() {
-  const logger = new Logger('bootstrap');
+  const serverConfig = config.get("server");
+  const logger = new Logger("bootstrap");
 
   const app = await NestFactory.create(AppModule);
-  const port = 3000;
-  await app.listen(port || 3000);
+
+  let port = process.env.PORT || serverConfig.port;
+
+
+  if (process.env.NODE_ENV === "development") {
+    app.enableCors();
+  } else {
+    app.enableCors({origin: serverConfig.origin})
+    logger.log(`Accepting request from origin ${serverConfig.origin}`)
+  }
+
+  await app.listen(port);
 
   logger.log(`Application listening on port ${port}`);
 }
+
 bootstrap();
